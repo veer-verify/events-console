6749b0339244513edefde751f362f39b0e30dedd
veer-verify <veer.verify@gmail.com>
2025-12-08 19:22:08 +0530

    env

--- src/environment.js
+++ src/environment.js
@@ -1,18 +1,22 @@
 const dev = "https://usstaging.ivisecurity.com";
-const event = "https://stagingmq.ivisecurity.com";
+
 const local = "http://192.168.0.225:3009";
 const localLogin = "http://192.168.0.225:3002";
 
+const prod = "http://prod.ivisecurity.com";
+
 
 export const environment = {
-    login_url: `${dev}/userDetails`,
-    site_url: `${dev}/vipsites`,
-    common_url: `${dev}/metadata`,
-    download_url: `${dev}/common`,
-    guard_monitoring_url: `${dev}/guard_monitoring`,
-    escalation_url: `${dev}/monitoring`,
-    event_image_url: `${dev}/dotimages/`,
-    event_process_url: `${dev}/events_data`,
-    events_url: `${event}/queueManagement`,
-    // monitoring_info_url: `${dev}`
+    // login_url: `${dev}/userDetails`,
+    // site_url: `${dev}/vipsites`,
+    // common_url: `${dev}/metadata`,
+    // guard_monitoring_url: `${dev}/guard_monitoring`,
+    // event_process_url: `${dev}/events_data`,
+    // events_url: `https://stagingmq.ivisecurity.com/queueManagement`,
+
+    login_url: `${prod}:5551/userDetails`,
+    site_url: `${prod}:5552/vipsites`,
+    common_url: `${prod}:8844/metadata`,
+    guard_monitoring_url: `${prod}:5553/guard_monitoring`,
+    events_url: `https://prodmq.ivisecurity.com/queueManagement`,
 };
